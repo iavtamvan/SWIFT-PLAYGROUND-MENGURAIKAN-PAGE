d01c001417d374072893dd19019c7099c8e9eb2d
iavtamvan <ade.fajr.ariav@gmail.com>
2020-07-12 14:18:41 +0700

    adding get Array from Result Movie DB's

--- Codelabs 1- Menguraikan Page.playground/Contents.swift
+++ Codelabs 1- Menguraikan Page.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
  
-let apiKey = "API-KEY"
+let apiKey = "0dde3e9896a8c299d142e214fcb636f8"
 let language = "en-US"
 let page = "1"
  
@@ -28,9 +28,58 @@ task.resume()
  
 struct Movies: Codable {
     let page: Int
-    let total_results: Int
-    let total_pages: Int
+    let totalResults: Int
+    let totalPages: Int
+    
+    let movies: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        
+        case movies = "results"
+    }
 }
+
+struct Movie: Codable {
+    let popularity: Double
+    let posterPath: String
+    let title: String
+    let genres: [Int]
+    let voteAverage: Double
+    let overview: String
+    
+    let releaseDate: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case popularity
+        case posterPath = "poster_path"
+        case title
+        case genres = "genre_ids"
+        case voteAverage = "vote_average"
+        case overview
+        case releaseDate = "release_date"
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let path = try container.decode(String.self, forKey: .posterPath)
+        let dateString = try container.decode(String.self, forKey: .releaseDate)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let date = dateFormatter.date(from: dateString)!
+        
+        popularity = try container.decode(Double.self, forKey: .popularity)
+        posterPath = "https://image.tmdb.org/t/p/w300\(path)"
+        title = try container.decode(String.self, forKey: .title)
+        genres = try container.decode([Int].self, forKey: .genres)
+        voteAverage = try container.decode(Double.self, forKey: .voteAverage)
+        overview = try container.decode(String.self, forKey: .overview)
+        
+        releaseDate = date
+       }
+}
+
  
 private func decodeJSON(data: Data) {
     let decoder = JSONDecoder()
@@ -38,6 +87,12 @@ private func decodeJSON(data: Data) {
     let movies = try! decoder.decode(Movies.self, from: data)
     
     print("PAGE: \(movies.page)")
-    print("TOTAL RESULTS: \(movies.total_results)")
-    print("TOTAL PAGES: \(movies.total_pages)")
+    print("TOTAL RESULTS: \(movies.totalResults)")
+    print("TOTAL PAGES: \(movies.totalPages)")
+    
+    movies.movies.forEach { movie in
+           print("TITLE: \(movie.title)")
+           print("POSTER: \(movie.posterPath)")
+           print("DATE: \(movie.releaseDate)")
+       }
 }
